7c408549a1178ca067b20346704af5b851667022
nasreddinendaconsulting <n.elghozi@ndaconsulting.tn>
2022-08-28 15:57:07 +0100

    start project

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,14 +14,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   final TextEditingController _textObfuscatedController = TextEditingController(text: '');
   final TextEditingController _textDeObfuscatedController = TextEditingController(text: '');
   final TextEditingController _filePathController = TextEditingController(text: '');
 
+  final ValueNotifier<bool> _isReadyToRun = ValueNotifier<bool>(false);
 
   @override
   void initState() {
     super.initState();
+
+    _filePathController.addListener(_isReadyToRunCheck);
+    _textObfuscatedController.addListener(_isReadyToRunCheck);
+  }
+
+  void _isReadyToRunCheck() {
+    _isReadyToRun.value = _filePathController.text.isNotEmpty && _textObfuscatedController.text.isNotEmpty;
+  }
+
+  @override
+  void dispose() {
+    _textObfuscatedController.dispose();
+    _textDeObfuscatedController.dispose();
+    _filePathController.dispose();
+    super.dispose();
   }
 
   @override
@@ -43,6 +60,10 @@ class _HomePageState extends State<HomePage> {
                     decoration: const InputDecoration(
                       contentPadding: EdgeInsets.all(9),
                       filled: true,
+                        labelText: 'Obfuscated StackTrace',
+                        hintText: 'Paste the obfuscated stacktrace here...',
+                      floatingLabelAlignment: FloatingLabelAlignment.center,
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
                     ),
                   ),
                 )
@@ -56,7 +77,10 @@ class _HomePageState extends State<HomePage> {
                         children: [
                           ElevatedButton(
                               onPressed: () async {
-                                FilePickerResult? result = await FilePicker.platform.pickFiles();
+                                FilePickerResult? result = await FilePicker.platform.pickFiles(
+                                  type: FileType.custom,
+                                  allowedExtensions: ['symbols']
+                                );
 
                                 if (result != null) {
                                   _filePathController.text = result.files.single.path ?? '';
@@ -69,6 +93,7 @@ class _HomePageState extends State<HomePage> {
                                 child: TextField(
                                   controller: _filePathController,
                                   keyboardType: TextInputType.text,
+                                  readOnly: true,
                                   maxLines: 1,
                                   decoration: const InputDecoration(
                                     contentPadding: EdgeInsets.all(9),
@@ -79,23 +104,25 @@ class _HomePageState extends State<HomePage> {
                           ),
                           Directionality(
                             textDirection: TextDirection.rtl,
-                            child: ElevatedButton.icon(
-                                onPressed: () async {
-                                  //var result = await Process.run('flutter', ['symbolize -d ${_filePathController.text}']);
-
-
-                                  Directory appDocDir = await getApplicationSupportDirectory();
-                                  String appDocPath = appDocDir.path;
-                                  File tempFile = File('$appDocPath/.obfuscate.txt');
-                                  tempFile.writeAsString(_textObfuscatedController.text);
-                                  var result = await Process.run('flutter symbolize', ['-d', _filePathController.text, '-i', tempFile.path]);
-                                  _textDeObfuscatedController.text = '${result.stdout}';
-                                },
-                                label: const Text('Process'),
-                              icon: const Icon(
-                                Icons.arrow_left,
-                                color: Colors.white,
-                            ),),
+                            child: ValueListenableBuilder<bool>(
+                              valueListenable: _isReadyToRun,
+                              builder: (context, isEnabled, child) {
+                                return ElevatedButton.icon(
+                                    onPressed: isEnabled ? () async {
+                                      Directory appDocDir = await getApplicationSupportDirectory();
+                                      String appDocPath = appDocDir.path;
+                                      File tempFile = File('$appDocPath/.obfuscate.txt');
+                                      tempFile.writeAsString(_textObfuscatedController.text);
+                                      var result = await Process.run('flutter', ['symbolize', '-d', _filePathController.text, '-i', tempFile.path]);
+                                      _textDeObfuscatedController.text = '${result.stdout}';
+                                    } : null,
+                                    label: const Text('Process'),
+                                  icon: const Icon(
+                                    Icons.arrow_left,
+                                    color: Colors.white,
+                                ),);
+                              }
+                            ),
                           )
                         ],
                       ),
@@ -110,6 +137,10 @@ class _HomePageState extends State<HomePage> {
                           decoration: const InputDecoration(
                             contentPadding: EdgeInsets.all(9),
                             filled: true,
+                              labelText: 'DeObfuscated StackTrace',
+                              hintText: 'Output...',
+                            floatingLabelAlignment: FloatingLabelAlignment.center,
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
                           ),
                         ),
                       )
