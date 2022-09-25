28bfb19d343c67bf0d3b379765e3046661b42ca6
gnassro <gnassro@outlook.com>
2022-09-25 15:06:47 +0100

    require flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,33 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_deobfuscate_crashlytics/flutter_not_installed_widget.dart';
 import 'package:flutter_deobfuscate_crashlytics/home_page.dart';
 import 'package:yaru/yaru.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  var result = await Process.run('command', ['-v', 'flutter', '>/dev/null', '2>&1 || {echo "required" >&2; exit 1;}'], runInShell: true);
+
+  runApp(MyApp(flutterInstalled: result.exitCode == 0,));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final bool flutterInstalled;
+
+  const MyApp({
+    Key? key,
+    required this.flutterInstalled
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "Flutter De-obfuscate Crashlytics's Stacktrace",
       debugShowCheckedModeBanner: false,
       home: YaruTheme(
-        child: HomePage(),
+        child: flutterInstalled ? const HomePage() : const  FlutterNotInstalledWidget(),
       ),
     );
   }
 }
+
